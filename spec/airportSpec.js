1090a189a1301c5f5a5ded9066b3820c5727d36d
durranee <durranee@hotmail.com>
2018-03-21 12:11:31 +0000

    first working test

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,1 +1,7 @@
+describe("Airport", function() {
+  var airport = new Airport();
 
+  it ("returns empty airport", function(){
+    expect(airport.get()).toEqual([]);
+  });
+});
--- src/airport.js
+++ src/airport.js
@@ -1,1 +1,8 @@
+function Airport(){
+ this._airport = [];
 
+  Airport.prototype.get = function () {
+    return this._airport;
+
+  };
+};
